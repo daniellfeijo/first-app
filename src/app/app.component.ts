f422a46168fac14def1863294d08faa586fea9ae
daniellfeijo <daniellfeijo@gmail.com>
2016-09-21 15:34:50 -0400

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>Inline Template</h1>
+    <fa-other></fa-other>
+    <fa-another>
+      <div>
+        <h1>Hello!</h1>
+        <p>World</p>
+      </div>
+    </fa-another> 
+    <fa-another>
+      <p>Something else</p>
+    </fa-another>    
+  `,
+  styles: [`
+      h1{
+        color:red;
+      }
+  `]
 })
 export class AppComponent {
   title = 'app works!';
